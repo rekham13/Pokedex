ae641a8f8ecdab26b33891501a34d3f51c21d700
rekha <rekhamanjunath37162@gmail.com>
2021-11-08 12:09:00 +0530

    group utility functions together to modularize

---
+++ src/util.js
@@ -0,0 +1,67 @@
+const fetchGet = async(url, options={}) => {
+    const response = await fetch(url,options);
+    const data = await response.json();
+
+    return data;
+}
+
+const getCapitalizedName = (str) => {
+    const capitalFirstLetter = str[0].toUpperCase();
+    const capitalizedStr = capitalFirstLetter + str.substr(1, str.length);
+
+    return capitalizedStr;
+}
+
+const getDetailedList = async(pokemonList) => {
+    const detailedList = [];
+
+    for await (const pokemon of pokemonList) {
+        const baseURL = process.env.REACT_APP_BASEURL;
+        const data = await fetchGet(baseURL + `/pokemon/${pokemon.name}`);
+        const { types, name,id } = data;
+
+        const properTypes = getProperTypes(types);
+
+        const details = {
+            id,
+            name,
+            properTypes
+        }
+        
+        detailedList.push(details);
+    }
+
+    return detailedList;
+}
+
+const getProperTypes = (typesArr) => {
+    const properTypes = typesArr.map(obj => {
+        return obj.type.name;
+    });
+
+    return properTypes;
+};
+
+const getImageSrcFor = (id) => {
+    const formattedForImageId = getFormattedForImageId(id);
+    const src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formattedForImageId}.png`;
+
+    return src;
+};
+
+const getFormattedForImageId = (id) => {
+    const strId = id.toString();
+    let formattedForImageId;
+
+    if (strId.length === 1) {
+        formattedForImageId = `00${id}`;
+    } else if (strId.length === 2) {
+        formattedForImageId = `0${id}`;
+    } else {
+        formattedForImageId = strId;
+    };
+
+    return formattedForImageId;
+}
+
+export { fetchGet, getCapitalizedName, getDetailedList,getImageSrcFor};
