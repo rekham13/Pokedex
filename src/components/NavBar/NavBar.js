65badd8af58ebd94bb2d870ce66e7012503e93d3
rekha <rekhamanjunath37162@gmail.com>
2021-11-09 15:52:25 +0530

    add navbar as a component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import { Navbar,Container } from "react-bootstrap"
+function NavBar(){
+    return(
+        <Navbar variant="dark" bg="dark" style={{marginBottom:"2rem"}}>
+            <Container style={{justifyContent:"center"}}>
+                <Navbar.Brand href="#">PokeDex</Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
+};
+
+export default NavBar;
