a16f219a896c8c16927749d93fcb458bda4f2837
rekha <rekhamanjunath37162@gmail.com>
2021-11-10 01:29:17 +0530

    bring conditional rendering logic outside the return statement with better readability

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,32 +1,56 @@
 import { useState,useEffect } from "react"
-import { Container, Row } from "react-bootstrap";
+import { Container, Row,Spinner } from "react-bootstrap";
+import FilterByType from "../../components/FilterByType/FilterByType";
 import PokemonList from "../../components/PokemonList";
+import PokePerPage from "../../components/PokePerPage/PokePerPage";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { fetchGet, getDetailedList } from "../../util";
 
-const baseURL = process.env.REACT_APP_BASEURL;
-
 function Home() {
     const [currentPageUrl, setCurrentPageUrl] = useState();
-    const [pokemonList, setPokemonList] = useState([]);
+    const [pokemonList, setPokemonList] = useState();
     const [searchResults, setSearchResults] = useState();
+    const [filteredResults, setFilteredResults] = useState();
 
     useEffect(() => {
-        fetchGet(baseURL + '/pokemon').then(async(data) => {
+        fetchGet('/pokemon').then(async(data) => {
             const pokemonList = data.results;
             const detailedPokemonList = await getDetailedList(pokemonList);
             setPokemonList(detailedPokemonList);
         })
-    }, [])
+    }, []);
+
+    let listToRender;
+    let listToSearch;
+
+    if(searchResults){
+        listToRender = searchResults;
+        listToSearch = pokemonList;
+    }else if(filteredResults){
+        listToRender = filteredResults;
+        listToSearch = filteredResults;
+    }else{
+        listToRender = pokemonList;
+        listToSearch = pokemonList;
+    }
     
-    return (
+    return ( 
+       (pokemonList === undefined) ?
+       <Container className="d-flex justify-content-center" fluid>
+            <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
+       </Container>:
         <Container>
-            <SearchBar pokemonList = {pokemonList} setSearchResults={setSearchResults} />
+            <SearchBar list = {listToSearch} setSearchResults={setSearchResults} />
+            <Container className="d-flex flex-column flex-sm-row justify-content-sm-between p-0">
+                <PokePerPage />
+                <FilterByType />
+            </Container>
             <Row>
-                {
-                    (searchResults !== undefined) ?  <PokemonList list={searchResults} /> : <PokemonList list={pokemonList} />
-                }
+                <PokemonList list={listToRender} />
             </Row>
+
         </Container> 
     )
 }
