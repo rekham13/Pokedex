72d61dc6534f94aebcbcf22953a6a4fd67b1f051
rekha <rekhamanjunath37162@gmail.com>
2021-11-09 15:32:07 +0530

    add pokemonList as component to modularize

---
+++ src/components/PokemonList.js
@@ -0,0 +1,36 @@
+import { Col, Card, Badge, Button  } from "react-bootstrap";
+import { getCapitalizedName,getImageSrcFor } from "../util";
+
+function PokemonList({list}){
+    console.log(list);
+    return(
+        list.map((pokemon, idx) => {
+            const capitalizedName = getCapitalizedName(pokemon.name);
+            const imgSrc = getImageSrcFor(pokemon.id);
+            return (
+                <Col xs={12} md={4} key={idx} style={{marginBottom:"1rem"}}>
+                    <Card>
+                        <Card.Img variant="top" src={imgSrc} />
+                        <Card.Body>
+                            <Card.Title>{ capitalizedName }</Card.Title>
+                            <Card.Text>
+                                {
+                                    pokemon.properTypes.map((type,idx) => {
+                                        return (
+                                            <Badge key={idx} pill bg='secondary' style={{ marginRight:'.5rem', padding:'.5rem'}}>
+                                                {type}
+                                            </Badge>
+                                        )
+                                    })
+                                }
+                            </Card.Text>
+                            <Button variant="primary">View More</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            )
+        })
+    )
+}
+
+export default PokemonList;
