8e7ee9671c69fa08e806c242e03df6ca94507592
rekha <rekhamanjunath37162@gmail.com>
2021-11-09 15:52:41 +0530

    render nav bar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './screens/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <React.Fragment>
+      <NavBar/>
+      <Home />
+    </React.Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );
