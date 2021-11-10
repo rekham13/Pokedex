e2b7c43c6550b25cff4f4ac283134c2f59f61e74
rekha <rekhamanjunath37162@gmail.com>
2021-11-10 11:39:22 +0530

    implement filter by type feature

--- src/components/FilterByType/FilterByType.js
+++ src/components/FilterByType/FilterByType.js
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from "react";
-import { DropdownButton, Dropdown} from "react-bootstrap";
+import { DropdownButton, Dropdown, Button} from "react-bootstrap";
 import { fetchGet } from "../../util";
 
-function FilterByType(){
+function FilterByType({list, setFilteredResults,setListToRender,setFilterApplied}){
     const [types, setTypes] = useState([]);
     const [filterSelected, setFilterSelected] = useState();
     
@@ -13,19 +13,52 @@ function FilterByType(){
         });
     },[]);
 
+
+
     return(
         <div className="d-flex justify-content-start p-0 mb-3">
-            <DropdownButton title="Filter by Type" variant="dark" menuVariant="dark" className="mb-3">
+            <DropdownButton title="Filter by Type" variant="dark" menuVariant="dark" className="mb-3" onSelect={onSelectHandler}>
+            <Dropdown.Header>
                 {
-                    types.map((type,idx)=>{
-                        return (
-                            <Dropdown.Item eventKey={type} key={idx}>{type}</Dropdown.Item>
-                        )
-                    })
+                    (!filterSelected) ?
+                    <Button disabled onClick={clearFilterHandler}>Clear filters</Button> :
+                    <Button onClick={clearFilterHandler}>Clear filters</Button>
                 }
+            </Dropdown.Header>
+                    {
+                        types.map((type,idx)=>{
+                            return (
+                                (filterSelected && filterSelected === type) ? 
+                                <Dropdown.Item eventKey={type} key={idx} active>{type}</Dropdown.Item>:
+                                <Dropdown.Item eventKey={type} key={idx}>{type}</Dropdown.Item>
+                            )
+                        })
+                    }
             </DropdownButton>
         </div>
     )
+
+    function onSelectHandler(type){
+        const results = list.filter(obj=>{
+            if(obj.properTypes.includes(type)){
+                return obj;
+            }
+        })
+
+        console.log(results);
+
+        setFilteredResults(results);
+        setFilterApplied(true);
+        setFilterSelected(type);
+        setListToRender(results);
+    }
+
+    function clearFilterHandler(){
+        setFilteredResults(undefined);
+        setFilterApplied(false);
+        setFilterSelected(undefined);
+        setListToRender(list);
+    }
 }
 
 export default FilterByType;
