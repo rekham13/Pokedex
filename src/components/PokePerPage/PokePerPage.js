c66feaa83a7e8766ba1dcb005e9503a1404c822e
rekha <rekhamanjunath37162@gmail.com>
2021-11-11 01:23:14 +0530

    remove unwanted logs

--- src/components/PokePerPage/PokePerPage.js
+++ src/components/PokePerPage/PokePerPage.js
@@ -1,14 +1,12 @@
 import { Dropdown,DropdownButton } from "react-bootstrap"
 
 function PokePerPage({limit, offset, setCurrentPageURL,setLimit}){
-    console.log(limit, offset);
-    const limits = [10,20,30,40,50];
+const limits = [10,20,30,40,50];
 
     return(
             <DropdownButton title="Poke's Per Page" variant="dark" menuVariant="dark" className="mb-3" onSelect={onSelectHandler}>
                 {
                     limits.map((numLimit,idx)=>{
-                        console.log(numLimit,limit);
                         return (
                             (numLimit === limit) ?
                             <Dropdown.Item eventKey={numLimit} key={idx} active>{numLimit}</Dropdown.Item>
