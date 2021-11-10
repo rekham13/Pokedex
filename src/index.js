64f2d753682b12515947e4138d92e4dbcb5ae3a2
rekha <rekhamanjunath37162@gmail.com>
2021-11-11 01:50:37 +0530

    add controller as the inital rendring element, remove unused declarations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './screens/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar/NavBar';
+import PokeDex from './CustomController/CustomController';
 
 ReactDOM.render(
     <React.Fragment>
-      <NavBar/>
-      <Home />
+      <PokeDex />
     </React.Fragment>,
   document.getElementById('root')
 );
