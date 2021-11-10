99174d9a09ff759b02f59b13a3e1635c62bb4681
rekha <rekhamanjunath37162@gmail.com>
2021-11-10 11:40:38 +0530

    changes to import filterbytype feature to work

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -7,48 +7,48 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import { fetchGet, getDetailedList } from "../../util";
 
 function Home() {
-    const [currentPageUrl, setCurrentPageUrl] = useState();
     const [pokemonList, setPokemonList] = useState();
-    const [searchResults, setSearchResults] = useState();
     const [filteredResults, setFilteredResults] = useState();
+    const [filterApplied, setFilterApplied] = useState(false);
 
+    const [listToRender, setListToRender] = useState();
+    
     useEffect(() => {
         fetchGet('/pokemon').then(async(data) => {
             const pokemonList = data.results;
             const detailedPokemonList = await getDetailedList(pokemonList);
             setPokemonList(detailedPokemonList);
+            setListToRender(detailedPokemonList);
         })
-    }, []);
+    }, []);       
 
-    let listToRender;
-    let listToSearch;
-
-    if(searchResults){
-        listToRender = searchResults;
-        listToSearch = pokemonList;
-    }else if(filteredResults){
-        listToRender = filteredResults;
-        listToSearch = filteredResults;
-    }else{
-        listToRender = pokemonList;
-        listToSearch = pokemonList;
-    }
-    
     return ( 
-       (pokemonList === undefined) ?
+       (pokemonList === undefined || listToRender === undefined) ?
        <Container className="d-flex justify-content-center" fluid>
             <Spinner animation="border" role="status">
                 <span className="visually-hidden">Loading...</span>
             </Spinner>
        </Container>:
         <Container>
-            <SearchBar list = {listToSearch} setSearchResults={setSearchResults} />
+            {
+                (filterApplied) ? 
+                <SearchBar list = {filteredResults} setListToRender={setListToRender} /> :
+                <SearchBar list = {pokemonList} setListToRender={setListToRender} />
+            }
             <Container className="d-flex flex-column flex-sm-row justify-content-sm-between p-0">
                 <PokePerPage />
-                <FilterByType />
+                <FilterByType list={pokemonList} setFilteredResults={setFilteredResults} setListToRender={setListToRender}  setFilterApplied={setFilterApplied} />
             </Container>
             <Row>
-                <PokemonList list={listToRender} />
+                {
+                    (listToRender.length === 0) ?
+                    <div className="d-flex justify-content-center" style={{width:"100%",height:"100vh"}}>
+                        <h2>
+                            Nothing to show
+                        </h2>
+                    </div> 
+                    : <PokemonList list={listToRender} />
+                }
             </Row>
 
         </Container> 
