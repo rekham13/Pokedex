a76a58c26ef4767b8b3851eb652b088578482864
rekha <rekhamanjunath37162@gmail.com>
2021-11-10 11:38:15 +0530

    remove react strict to prevent 2 renders at the begining

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
 
 ReactDOM.render(
-  <React.StrictMode>
     <React.Fragment>
       <NavBar/>
       <Home />
-    </React.Fragment>
-  </React.StrictMode>,
+    </React.Fragment>,
   document.getElementById('root')
 );
